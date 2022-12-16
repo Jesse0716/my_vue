105c23b8c072e995871a6312c39cdaf0eb2449cb
Jesse_hui <jesse_xianghui@163.com>
2022-12-16 15:19:23 +0800

    21_src_github搜索案例_vue-resource

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,37 @@
 <template>
   <div class="container">
-    <my-search />
-    <my-list />
+    <my-category title="美食">
+      <img src="https://v2.cn.vuejs.org/images/logo.svg" alt="">
+    </my-category>
+    <my-category :title="游戏">
+      <ul>
+        <li v-for="(item,index) in games" :key="index">{{item}}</li>
+      </ul>
+    </my-category>
+    <my-category :title="电影">
+      <video controls src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"></video>
+    </my-category>
   </div>
 </template>
 
 <script>
-import MyList from './components/MyList'
-import MySearch from './components/MySearch'
+import MyCategory from './components/MyCategory.vue'
 export default {
-  components: { MySearch, MyList },
-  name: 'App'
+  components: { MyCategory },
+  name: 'App',
+  data() {
+    return {
+      foods: ['火锅', '烧烤', '小龙虾', '牛排'],
+      games: ['超级玛丽', '冒险岛', '吃鸡', 'LOL'],
+      films: ['《教父》', '《无双》', '《活着》', '《上帝未死》']
+    }
+  }
 }
 </script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: space-around;
+}
+</style>
